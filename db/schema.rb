c513ba6906ce5075fc8fe7dabc44184f7140b3ba
naoki-nakajima <vsme@i.softbank.jp>
2020-11-19 22:19:34 +0900

    gem導入。hamlの導入。dbの編集

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_044126) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "vendor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
