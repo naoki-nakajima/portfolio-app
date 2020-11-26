79b0a54040cf1edc51de7aae681df1759e1381f5
naoki-nakajima <vsme@i.softbank.jp>
2020-11-26 22:04:50 +0900

    greetingのアニメーションの実装

--- app/assets/javascripts/plugin.js
+++ app/assets/javascripts/plugin.js
@@ -1,3 +1,53 @@
+$(function () {
+  $('.greeting').textillate({
+    loop: true,                 //ループ
+    minDisplayTime: 3000,       //テキストが置き換えられるまでの時間
+    // initialDelay: 1000,         //遅延時間
+    autoStart: true,            //アニメーション自動スタート
+    // フェードイン
+    in: {
+      effect: 'fadeIn',         //アニメーション効果、エフェクトの種類
+      delayScale: 1.5,          //遅延時間の指定
+      delay: 50,                //文字ごとの遅延時間
+      sync: false,              //『true』全ての文字に同時に適応される
+      shuffle: false            //ランダム
+    },  
+    // フェードアウト
+    out: {
+      effect: 'fadeOut',  
+      delayScale: 1.5,  
+      delay: 50,  
+      sync: false,  
+      shuffle: false
+    }
+  });
+
+  $('.details').textillate({
+    loop: true,                 
+    minDisplayTime: 3000,       
+    autoStart: true,            
+    
+    in: {
+      effect: 'fadeIn',         
+      delayScale: 1.5,          
+      delay: 50,                
+      sync: false,              
+      shuffle: false            
+    },  
+    out: {
+      effect: 'fadeOut',  
+      delayScale: 1.5,  
+      delay: 50,  
+      sync: false,  
+      shuffle: false
+    }
+  });
+ })
+
+
+
+
+
 $(function() {
   var offset = $('.header').offset();
 
