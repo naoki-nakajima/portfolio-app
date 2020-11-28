30d5bf29802df89adac1e1cb92ecf0642b497271
naoki-nakajima <vsme@i.softbank.jp>
2020-11-28 22:44:49 +0900

    jsの修正

--- app/assets/javascripts/plugin.js
+++ app/assets/javascripts/plugin.js
@@ -1,62 +1,26 @@
-$(function () {
-  $('.greeting').textillate({
-    loop: true,                 //ループ
-    minDisplayTime: 3000,       //テキストが置き換えられるまでの時間
-    // initialDelay: 1000,         //遅延時間
-    autoStart: true,            //アニメーション自動スタート
-    // フェードイン
-    in: {
-      effect: 'fadeIn',         //アニメーション効果、エフェクトの種類
-      delayScale: 1.5,          //遅延時間の指定
-      delay: 50,                //文字ごとの遅延時間
-      sync: false,              //『true』全ての文字に同時に適応される
-      shuffle: false            //ランダム
-    },  
-    // フェードアウト
-    out: {
-      effect: 'fadeOut',  
-      delayScale: 1.5,  
-      delay: 50,  
-      sync: false,  
-      shuffle: false
+var chartJsPluginCenterLabel = {
+  labelShown: false,
+
+  afterRender: function (chart) {
+    if (this.labelShown) {
+      return;
     }
-  });
+    // ラベルの HTML
+    var value = chart.data.datasets[0].data[0];
+    var labelBox = document.createElement('div');
+    labelBox.classList.add('label-box');
+    labelBox.innerHTML = '<div class="label">'
+      + value
+      + '<span class="per">%</span>'
+      + '</div>';
+    // ラベル上部の padding
+    var paddingTop = Math.round(chart.chartArea.top);
+    labelBox.setAttribute('style', 'padding-top:' + paddingTop + 'px');
+    // ラベル描画
+    var canvas = chart.ctx.canvas;
+    canvas.parentNode.insertBefore(labelBox, canvas.nextElementSibling);
+  },
+};
 
-  $('.details').textillate({
-    loop: true,                 
-    minDisplayTime: 3000,       
-    autoStart: true,            
-    
-    in: {
-      effect: 'fadeIn',         
-      delayScale: 1.5,          
-      delay: 50,                
-      sync: false,              
-      shuffle: false            
-    },  
-    out: {
-      effect: 'fadeOut',  
-      delayScale: 1.5,  
-      delay: 50,  
-      sync: false,  
-      shuffle: false
-    }
-  });
- })
-
-
-
-
-
-$(function() {
-  var offset = $('.header').offset();
-
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > offset.top) {
-      $('.header').addClass('h_fixed');
-    }
-    else {
-      $('.header').removeClass('h_fixed');
-    }
-  });
-});
+// 上記プラグインの有効化
+Chart.plugins.register(chartJsPluginCenterLabel);
